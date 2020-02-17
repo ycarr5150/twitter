84833e72d6b7c3efab1f1d24f09937fe2e1ae98d
Yazmin Carrillo <ycarrillo@csumb.edu>
2020-02-16 21:33:47 -0800

    Basic Requirements

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Yazmin Carrillo on 2/16/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
